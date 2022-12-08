78c9feef58134ea7e2c9f9c92d92773a05031be0
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-12-09 01:00:04 +0900

    학사정보 모듈에서 가져오기

--- test/schedule_T.js
+++ test/schedule_T.js
@@ -1,7 +1,9 @@
+const scheduleInfo = require("./haksa");
+
 function schedule_test(text, status){
     switch(status){
         case 13:
-            if(text === '8/4는 2학기 수강신청입니다.'){
+            if(text === `8/4는 ${scheduleInfo[text]}입니다.`){
                 console.log("--8/4 학사안내 테스트 통과");
             }else{
                 console.log("--8/4 학사안내 테스트 실패");
