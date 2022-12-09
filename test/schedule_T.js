e7a1da6b5f1cb2116a18b231fb96935b5245ff04
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-12-09 21:35:50 +0900

    안내 메시지 개선

--- test/schedule_T.js
+++ test/schedule_T.js
@@ -4,14 +4,14 @@ function schedule_test(text, status) {
   switch (status) {
     case 2:
       if (text === `${scheduleInfo['8/4']}`) {
-        console.log('--8/4 학사안내 테스트 통과');
+        console.log('---8/4 학사안내 테스트 통과');
       } else {
         console.log('★★★★★★ 8/4 학사안내 테스트 실패');
       }
       break;
     case 4:
       if (text === `${scheduleInfo['10/15']}`) {
-        console.log('--10/15 학사안내 테스트 통과');
+        console.log('---10/15 학사안내 테스트 통과');
       } else {
         console.log('★★★★★★ 10/15 학사안내 테스트 실패');
       }
